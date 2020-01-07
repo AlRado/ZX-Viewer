3f88ed5851d44ce2af21653f61ccea8f5dae11a7
alrado <alrado08@gmail.com>
2020-01-08 00:03:57 +0300

    Added license and minor changes

--- ZXViewer.py
+++ ZXViewer.py
@@ -1,33 +1,55 @@
+# M5Stack ZX-Spectrum Screens Viewer v.0.2
+# 
+# The script loads standard ZX-Spectrum .scr images via HTTP and displays them on the screen. 
+# In version 0.2 flickering is not supported yet.
+# An excellent collection of the best screens of ZX-Spectrum is located at the Dmitry Ponomarev's website - https://zxart.ee
+# 
+# Tested on UIFlow V1.4.3
+
+# 
+# The MIT License (MIT)
+# 
+#  Copyright © 2020 Alexey Radyuk
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+# associated documentation files (the «Software»), to deal in the Software without restriction, including 
+# without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+# of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED «AS IS», WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+# THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+
 import usocket as socket
 import ujson
 from m5stack import lcd
 
-# press the button A if the picture is not loaded
-# press the button B to scroll back
+# press the button A to scroll back
+# press the button B to reload screen
 # press the button С to scroll forward
 
 # the number of pictures in the top from which to start the show
-START_IX = 5
-
+START_IX = 0
 LOAD_LIMIT = 5
 PICTURE_MIN_RATING = 0
 
-WIDTH = 256
-HEIGHT = 192
 ZX_ART_HOST = "https://zxart.ee/file/id:%s/filename:tmp"
 ZX_ART_API_REQUEST = "https://zxart.ee/api/types:zxPicture/export:zxPicture/language:eng/start:%s/limit:%s/order:votes,desc/filter:zxPictureMinRating=%s;zxPictureType=standard;"
-TOP_ZX_ART_SCREENS = []
+GET_REQUEST = 'GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n'
 TEMP_FILE = 'temp_file.scr'
 DATA_FILE = 'data.json'
 
 #  Color scheme - Pulsar
-PALETTE = [
-    0, 205, 13434880, 13435085, 52480, 52685, 13487360, 13487565, 0, 255, 16711680, 16711935, 65280, 65535, 16776960,
-    16777215
-]
+PALETTE = [0, 205, 13434880, 13435085, 52480, 52685, 13487360, 13487565, 0, 255, 16711680, 16711935, 65280, 65535, 16776960, 16777215]
 
 ix = 0
 isTryingLoad = True
+screens = []
 
 
 def split_list(l, s):
@@ -61,7 +83,7 @@ def http_getFile(url, file):
     addr = socket.getaddrinfo(host, 80)[0][-1]
     s = socket.socket()
     s.connect(addr)
-    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    s.send(bytes(GET_REQUEST % (path, host), 'utf8'))
     while isTryingLoad:
         data = s.recv(200)
         if data:
@@ -103,8 +125,6 @@ def convertToScreen(data):
                 continue
             lcd.pixel(32 + x, 24 + y, PALETTE[pixrow[x]])
 
-    lcd.print("<< >>", 270, 0)
-
 
 def readBits(data, y, col):
     block = int(y / 64)
@@ -133,23 +153,23 @@ def loadImage():
     global ix, START_IX, LOAD_LIMIT, isTryingLoad
     isTryingLoad = True
     if ix < 0:
-        ix = len(TOP_ZX_ART_SCREENS) - 1
+        ix = len(screens) - 1
 
-    if ix > len(TOP_ZX_ART_SCREENS) - 1:
+    if ix > len(screens) - 1 :
         START_IX += LOAD_LIMIT
         appendScreensIds()
 
     lcd.clear()
     lcd.setCursor(0, 0)
     lcd.setColor(lcd.WHITE)
-    lcd.print('Load #%s: %s' % (START_IX + ix + 1, TOP_ZX_ART_SCREENS[ix][1]))
+    lcd.print('Top of ZXART.EE: #%s %s' % (ix + 1, screens[ix][1]))
 
     data = open(TEMP_FILE, 'wb')
-    addr = ZX_ART_HOST % TOP_ZX_ART_SCREENS[ix][0]
+    addr = ZX_ART_HOST % screens[ix][0]
     http_getFile(addr, data)
     loadedData = open(TEMP_FILE, 'rb').read()
     convertToScreen(loadedData)
-    lcd.print("SCR-file got from the top of ZXART.EE", 24, 224)
+    lcd.print("        <<  A        Reload - B         C >>", 0, 224)
 
 
 def appendScreensIds():
@@ -163,23 +183,24 @@ def appendScreensIds():
     for data in scrData:
         id = data['id']
         title = data['title'][0:30]
-        TOP_ZX_ART_SCREENS.append((id, title))
+        screens.append((id, title))
 
 
 def printLoadingBreak():
     lcd.clear()
-    lcd.print("Loading BREAK, press B or C", 24, 224)
+    lcd.print("Loading BREAK, press any key", 24, 224)
 
 
 def on_A_wasPressed():
-    global isTryingLoad
-    isTryingLoad = False
-    printLoadingBreak()
+    global ix
+    ix -= 1
+    loadImage()
 
 
 def on_B_wasPressed():
-    global ix
-    ix -= 1
+    global isTryingLoad
+    isTryingLoad = False
+    printLoadingBreak()
     loadImage()
 
 
@@ -189,9 +210,9 @@ def on_C_wasPressed():
     loadImage()
 
 
-buttonA.wasPressed(on_A_wasPressed)
-buttonB.wasPressed(on_B_wasPressed)
-buttonC.wasPressed(on_C_wasPressed)
+btnA.wasPressed(on_A_wasPressed)
+btnB.wasPressed(on_B_wasPressed)
+btnC.wasPressed(on_C_wasPressed)
 
 appendScreensIds()
 loadImage()
